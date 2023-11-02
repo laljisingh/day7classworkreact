1bc5c6309aa1cfa78c0d1de9b1920925bb531ddf
laljisingh <lalji7411@gmail.com>
2023-11-02 19:30:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './component/Footer';
+import Navbar from './component/Navbar';
+import Body from './component/body';
 
 function App() {
+  const data1 = {
+    data : "It's amazing how pervasive food is. Every second commercial is for food. Every second TV episode takes place around a meal. In the city, you can't go ten feet without seeing or smelling a restaurant. There are 20 foot high hamburgers up on billboards. I am acutely aware of food, and its omnipresence is astounding.",
+    name:"Adam Scott"
+  };
+  const data2 = {
+    data : "It's amazing how pervasive food is. Every second commercial is for food. Every second TV episode takes place around a meal. In the city, you can't go ten feet without seeing or smelling a restaurant. There are 20 foot high hamburgers up on billboards. I am acutely aware of food, and its omnipresence is astounding.",
+    name:"Adam Scott"
+  };
+  const data3 = {
+    data : "It's amazing how pervasive food is. Every second commercial is for food. Every second TV episode takes place around a meal. In the city, you can't go ten feet without seeing or smelling a restaurant. There are 20 foot high hamburgers up on billboards. I am acutely aware of food, and its omnipresence is astounding.",
+    name:"Adam Scott"
+  };
+  const data4 = {
+    data : "It's amazing how pervasive food is. Every second commercial is for food. Every second TV episode takes place around a meal. In the city, you can't go ten feet without seeing or smelling a restaurant. There are 20 foot high hamburgers up on billboards. I am acutely aware of food, and its omnipresence is astounding.",
+    name:"Adam Scott"
+  };
+  const data5 = {
+    data : "It's amazing how pervasive food is. Every second commercial is for food. Every second TV episode takes place around a meal. In the city, you can't go ten feet without seeing or smelling a restaurant. There are 20 foot high hamburgers up on billboards. I am acutely aware of food, and its omnipresence is astounding.",
+    name:"Adam Scott"
+  };
+  const data6 = {
+    data : "It's amazing how pervasive food is. Every second commercial is for food. Every second TV episode takes place around a meal. In the city, you can't go ten feet without seeing or smelling a restaurant. There are 20 foot high hamburgers up on billboards. I am acutely aware of food, and its omnipresence is astounding.",
+    name:"Adam Scott"
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Body props={data1}></Body>
+      <Body props={data2}></Body>
+      <Body props={data3}></Body>
+      <Body props={data4}></Body>
+      <Body props={data5}></Body>
+      <Body props={data6}></Body>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/component/body.js
@@ -0,0 +1,16 @@
+function Body({ props }) {
+    const stl = {
+        marginTop:"80px",
+        width:"80%"
+    };
+    return (
+        <>
+            <div style={stl} class="w-100 p-9 pt-5 mt-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-10 dark:bg-gray-800 dark:border-gray-700 hover:bg-cyan-200 p-4 transition duration-300">
+                <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">{props.data}</h5>
+                <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">{props.name}</p>
+            </div>
+
+        </>
+    );
+}
+export default Body;
