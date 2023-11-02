a55d4a89263c706284a4e8aac43b875aa98ff0c3
laljisingh <lalji7411@gmail.com>
2023-11-02 23:03:02 +0530

    modify

--- src/App.js
+++ src/App.js
@@ -1,42 +1,20 @@
 import './App.css';
 import Footer from './component/Footer';
 import Navbar from './component/Navbar';
-import Body from './component/body';
+import Body from './component/Body';
+import Quotes from './component/quotes.json';
 
 function App() {
-  const data1 = {
-    data : "It's amazing how pervasive food is. Every second commercial is for food. Every second TV episode takes place around a meal. In the city, you can't go ten feet without seeing or smelling a restaurant. There are 20 foot high hamburgers up on billboards. I am acutely aware of food, and its omnipresence is astounding.",
-    name:"Adam Scott"
-  };
-  const data2 = {
-    data : "It's amazing how pervasive food is. Every second commercial is for food. Every second TV episode takes place around a meal. In the city, you can't go ten feet without seeing or smelling a restaurant. There are 20 foot high hamburgers up on billboards. I am acutely aware of food, and its omnipresence is astounding.",
-    name:"Harry poter"
-  };
-  const data3 = {
-    data : "It's amazing how pervasive food is. Every second commercial is for food. Every second TV episode takes place around a meal. In the city, you can't go ten feet without seeing or smelling a restaurant. There are 20 foot high hamburgers up on billboards. I am acutely aware of food, and its omnipresence is astounding.",
-    name:"Adam Scott"
-  };
-  const data4 = {
-    data : "It's amazing how pervasive food is. Every second commercial is for food. Every second TV episode takes place around a meal. In the city, you can't go ten feet without seeing or smelling a restaurant. There are 20 foot high hamburgers up on billboards. I am acutely aware of food, and its omnipresence is astounding.",
-    name:"Adam Scott"
-  };
-  const data5 = {
-    data : "It's amazing how pervasive food is. Every second commercial is for food. Every second TV episode takes place around a meal. In the city, you can't go ten feet without seeing or smelling a restaurant. There are 20 foot high hamburgers up on billboards. I am acutely aware of food, and its omnipresence is astounding.",
-    name:"Adam Scott"
-  };
-  const data6 = {
-    data : "It's amazing how pervasive food is. Every second commercial is for food. Every second TV episode takes place around a meal. In the city, you can't go ten feet without seeing or smelling a restaurant. There are 20 foot high hamburgers up on billboards. I am acutely aware of food, and its omnipresence is astounding.",
-    name:"Adam Scott"
-  };
+
   return (
     <div className="App">
       <Navbar></Navbar>
-      <Body props={data1}></Body>
-      <Body props={data2}></Body>
-      <Body props={data3}></Body>
-      <Body props={data4}></Body>
-      <Body props={data5}></Body>
-      <Body props={data6}></Body>
+      {
+        Quotes.quote.map((ele)=>(
+          <Body text={ele.quote} author = {ele.author}></Body>
+        ))
+
+      }
       <Footer></Footer>
     </div>
   );
